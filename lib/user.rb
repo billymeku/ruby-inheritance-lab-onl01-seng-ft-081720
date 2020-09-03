b51e74efcdacc2ffb76ca3c19ef979c8a75a2da7
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-03 04:57:07 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,9 +1,9 @@
 class User 
   
   attr_accessor :first_name, :last_name
-  @@KNOWLEDGE = []
+  @knowledge = []
   
   def learn(string)
-    @@KNOWLEDGE << string 
+   @knowledge << string 
   end 
 end 
